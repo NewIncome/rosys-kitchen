8ea6c5ffe4d09b1a1af8d5f6d74340946a4b7d9b
NewIncome <alfredo_cardenas@live.com>
2021-03-01 20:21:13 -0600

    Configure webpack server and a last step to use bootstrap
    - import bootstrap styles to my style.scss

--- webpack.config.js
+++ webpack.config.js
@@ -41,4 +41,9 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+  },
 };
