7eacb992c924d82838da4aef125064af9840b5ff
NewIncome <alfredo_cardenas@live.com>
2021-03-04 13:14:24 -0600

    minor js change, didn't affect but keeps consistency with bootstrap

--- src/index.js
+++ src/index.js
@@ -7,9 +7,15 @@ document.addEventListener('DOMContentLoaded', () => {
   const navmenu = document.querySelector('.navbar-collapse');
   const navlink = document.querySelectorAll('.nav-link');
 
+  console.log(button.attributes[5]);
+  button.setAttribute('aria-expanded', 'true');
+  // button.attributes[aria-expanded] = 'true';
+  console.log(button.attributes["aria-expanded"]);
+
   navlink.forEach(link => {
     link.addEventListener('click', () => {
       button.className = 'navbar-toggler collapsed';
+      button.setAttribute('aria-expanded', 'false');
       navmenu.className = 'navbar-collapse collapse';
     });
   });
